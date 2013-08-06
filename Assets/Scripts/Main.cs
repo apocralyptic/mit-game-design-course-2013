5cda516fe535a264c1868cdd89538d1804728d79
Nicholas Arcolano <apocralyptic@gmail.com>
2013-08-06 13:33:56 -0400

    Changed colliders & rigidbodies.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,7 +6,7 @@ public class Main : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		GameObject gEnemy = (GameObject)Instantiate(Resources.Load("Enemy"));
-		gEnemy.rigidbody.position = new Vector3(10,10,10);
+		gEnemy.transform.position = new Vector3(10,10,10);
 		CurveMotion mEnemy = (CurveMotion)gEnemy.GetComponent(typeof(CurveMotion));
 		mEnemy.functionType = "linear";//"quadratic";
 		mEnemy.moveDirection = -1;
@@ -15,8 +15,8 @@ public class Main : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown ("space")){
-			GameObject p = (GameObject)Instantiate(Resources.Load("Enemy"));
-			p.rigidbody.position = new Vector3(0,0,10);
+			GameObject p = (GameObject)Instantiate(Resources.Load("Projectile"));
+			p.transform.position = new Vector3(0,0,10);
 			CurveMotion mP = (CurveMotion)p.GetComponent(typeof(CurveMotion));
 			mP.functionType = "linear";//"quadratic";
 			mP.moveDirection = 1;
